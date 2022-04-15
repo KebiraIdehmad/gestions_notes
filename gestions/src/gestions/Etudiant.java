bf492a2f2519d66534ccf061c345df08e2e108de
dell <dell@10.30.94.2>
2022-04-15 15:38:05 +0000

    le constructeur

--- gestions/src/gestions/Etudiant.java
+++ gestions/src/gestions/Etudiant.java
@@ -7,4 +7,9 @@ public class Etudiant implements Comparable<Etudiant> {
 	private int id;
 	private String nom;
 	private double note;
-}
+	public Etudiant (id,nom) {
+		this.id = id;
+		this.nom = nom;
+		this.note = 10;
+	}
+}	
